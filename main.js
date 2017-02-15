56ac5eca039b17f5b7c1395a59edbd981bbb4aee
Yaroslav Kanarev <ykanarev@spbtv.com>
2017-02-15 12:55:07 +0300

    test

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function getArgValue(name)
+{
+    url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function httpGet(url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
